0d3ef10e7a305ab98073b20a9dc80393cd54bdfa
Narin <hongchhiev@gmail.com>
2023-03-18 14:05:52 +0700

    NFAtoDFA

--- src/helper/helper.py
+++ src/helper/helper.py
@@ -120,3 +120,105 @@ class FiniteAutomata:
     
 
     
+# NFA to DFA
+class NFA:
+    def __init__(self, states, alphabet, transitions, initial_state, accepting_states):
+        self.states = set(states)
+        self.alphabet = set(alphabet)
+        self.transitions = transitions
+        self.initial_state = initial_state
+        self.accepting_states = set(accepting_states)
+
+    def epsilon_closure(self, states):
+        """
+        Returns the epsilon closure of the given set of states.
+        """
+        closure = set(states)
+        stack = list(states)
+        while stack:
+            state = stack.pop()
+            for epsilon_transition in self.transitions.get((state, None), []):
+                if epsilon_transition not in closure:
+                    closure.add(epsilon_transition)
+                    stack.append(epsilon_transition)
+        return closure
+
+    def to_dfa(self):
+        """
+        Converts the NFA to a DFA using the subset construction algorithm.
+        """
+        dfa_states = []
+        dfa_transitions = {}
+        dfa_initial_state = frozenset(self.epsilon_closure([self.initial_state]))
+        dfa_accepting_states = set()
+        unmarked_states = [dfa_initial_state]
+
+        while unmarked_states:
+            state_set = unmarked_states.pop()
+            dfa_states.append(state_set)
+
+            for symbol in self.alphabet:
+                next_states = set()
+                for state in state_set:
+                    for transition in self.transitions.get((state, symbol), []):
+                        next_states |= self.epsilon_closure([transition])
+                if next_states:
+                    next_state_set = frozenset(next_states)
+                    dfa_transitions[(state_set, symbol)] = next_state_set
+                    if next_state_set not in dfa_states:
+                        unmarked_states.append(next_state_set)
+
+            if any(state in self.accepting_states for state in state_set):
+                dfa_accepting_states.add(state_set)
+
+        return DFA(
+            alphabet=self.alphabet,
+            states=dfa_states,
+            transitions=dfa_transitions,
+            initial_state=dfa_initial_state,
+            accepting_states=dfa_accepting_states
+        )
+
+class DFA:
+    def __init__(self, alphabet, states, transitions, initial_state, accepting_states):
+        self.alphabet = alphabet
+        self.states = states
+        self.transitions = transitions
+        self.initial_state = initial_state
+        self.accepting_states = accepting_states
+
+    def accepts(self, input_string):
+        """
+        Determines whether the DFA accepts the given input string.
+        """
+        current_state = self.initial_state
+        for symbol in input_string:
+            current_state = self.transitions.get((current_state, symbol))
+            if current_state is None:
+                return False
+        return current_state in self.accepting_states
+
+# Define the NFA from the given example
+states = ["q1", "q2", "q3"]
+alphabet = ["a", "b"]
+transitions = {
+    ("q1", "a"): {"q1", "q2"},
+    ("q1", "b"): {"q1"},
+    ("q2", "a"): set(),
+    ("q2", "b"): {"q3"}
+}
+initial_state = "q1"
+accepting_states = ["q2"]
+nfa = NFA(states, alphabet, transitions, initial_state, accepting_states)
+
+# Convert NFA to DFA
+dfa = nfa.to_dfa()
+
+
+# Print the resulting DFA
+print("\n_____Here we have our DFA_____\n")
+print("Alphabet: ", dfa.alphabet)
+print("States: ", dfa.states)
+print("Transitions: ", dfa.transitions)
+print("Initial state: ", dfa.initial_state)
+print("Accepting states: ", dfa.accepting_states)
